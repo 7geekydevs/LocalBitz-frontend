ec1fa45e29a3442c8225f195bbb1c180ae0a85dd
Akshay <akshayjoshi4770@gmail.com>
2021-11-02 16:17:52 +0530

    added sign up page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import landing from './pages/Landing/landing';
+import register from './pages/Register/register';
 
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Switch>
-        <Route path="/" component={landing}></Route>
+        {/* <Route path="/" component={landing}></Route>  */}
+        <Route path="/register" component={register}></Route>
+
       </Switch>
       </BrowserRouter>
     </div>
