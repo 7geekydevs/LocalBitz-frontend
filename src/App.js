ffde272b6d6cdf8945036b605f296b9afc58246c
Rohit_Joshi <rohitjoshi632001@gmail.com>
2021-11-03 17:58:56 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import './App.css';
 import { Route, Switch } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import landing from './pages/Landing/landing';
-import register from './pages/Register/register';
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+    <BrowserRouter>
       <Switch>
-        {/* <Route path="/" component={landing}></Route>  */}
-        <Route path="/register" component={register}></Route>
-
+        <Route path="/" component={landing}></Route>
       </Switch>
       </BrowserRouter>
     </div>
+  
   );
 }
 
